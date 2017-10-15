0aea08dee8b300e67223bda9664147dbf825a498
Simon Myunggun Seo <simonseo.doubles@gmail.com>
2017-10-15 07:58:50 -0400

    port Alice's new form handler to Flask

--- front-end/app/views.py
+++ front-end/app/views.py
@@ -4,14 +4,16 @@ from flask import render_template, redirect, url_for, request
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html")
+	return render_template("index.html")
 
 @app.route('/submit', methods=['GET', 'POST'])
 def submit():
 	result = ''
 	import cgi, cgitb
-	# import psycopg2
 	from datetime import datetime as dt
+	from threading import Timer
+	from twilio.rest import Client
+	from envparse import env
 
 	cgitb.enable()
 
@@ -22,11 +24,9 @@ def submit():
 	result += ("</head>")
 	result +=  ( "<body>")
 
-	# Create instance of FieldStorage 
-	form = cgi.FieldStorage()
-
 	number = request.form['phone']
 	datetime = request.form['sendTime']
+	datetime = dt.strptime(datetime, '%Y-%m-%dT%H:%M')
 	occasion = ""
 	message = ""
 	giphyurl = ""
@@ -37,9 +37,53 @@ def submit():
 	if request.form['url']:
 		giphyurl = request.form['url']
 
-	result += ','.join([number, datetime, occasion, message, giphyurl])
+
+	env.read_envfile()
+	account_sid = env.str('ACCOUNT_SID')
+	auth_token = env.str('AUTH_TOKEN')
+
+	result += ','.join([number, str(datetime), occasion, message, giphyurl])
+
+	def sendMessage(recipient, msg = "", gif = ""):
+		# param: strings
+		# must enter either msg or gif or both
+		client = Client(account_sid, auth_token)
+
+		if msg == "": 
+			message = client.messages.create(
+					recipient,
+					from_ = "+12013477040",
+					media_url = gif)
+		elif gif == "":
+			message = client.messages.create(
+					recipient,
+					body = msg,
+					from_ = "+12013477040")
+		else: 
+			message = client.messages.create(
+					recipient,
+					body = msg,
+					from_ = "+12013477040",
+					media_url = gif)
+
+
+	x = dt.today()
+	y = datetime
+	delta_t = y - x
+	secs = delta_t.seconds + 1
+
+	timer = Timer(secs, sendMessage, [number, message, giphyurl])
+	timer.start()
 
 
 	result += ("</body>")
 	result += ("</html>")
 	return result
+
+
+
+
+
+
+
+print("</body>")
